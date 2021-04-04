72ef94828ecb0221f1f46e70fd7bc37564b84099
zerroage <zerroage@gmail.com>
2021-04-04 20:49:59 +0200

    Reformat code and optimize imports

--- dutch-tools.py
+++ dutch-tools.py
@@ -1,34 +1,17 @@
-import itertools
-import random
-import re
-import string
-import traceback
-from collections import OrderedDict
-# noinspection PyUnresolvedReferences
-from datetime import date, datetime, timedelta
-# noinspection PyUnresolvedReferences
-from dateutil.relativedelta import relativedelta
-# noinspection PyUnresolvedReferences
-from fractions import Fraction
-# noinspection PyUnresolvedReferences
-from math import *
-from time import gmtime, strftime
-from functools import reduce
-# noinspection PyUnresolvedReferences
-import sublime
-# noinspection PyUnresolvedReferences
-import sublime_plugin
-import time
-import bs4
-# from urllib.request import urlopen
-import requests
 import json
+import re
+
+import bs4
+import requests
+import sublime
+import sublime_plugin
+
 
 def print_in_panel(view, edit, text):
     panel = view.window().find_output_panel("dutch_tools_panel")
     if not panel:
         panel = view.window().create_output_panel("dutch_tools_panel")
-        
+
     view.window().run_command('show_panel', {"panel": 'output.dutch_tools_panel'})
 
     panel.insert(edit, panel.size(), text + "\n")
@@ -52,7 +35,6 @@ def get_transcription(word):
 
 
 def insert_transcription(view, edit):
-    word = ""
     selection = view.sel()
 
     if selection:
@@ -77,14 +59,13 @@ def insert_transcription(view, edit):
                 view.insert(edit, line.end(), transcription)
 
 
-        
 def request(action, **params):
     return {'action': action, 'params': params, 'version': 6}
 
 
 def invoke(action, **params):
-    requestJson = json.dumps(request(action, **params)).encode('utf-8')
-    r = requests.post("http://localhost:8765", requestJson)
+    request_json = json.dumps(request(action, **params)).encode('utf-8')
+    r = requests.post("http://localhost:8765", request_json)
     response = json.loads(r.text)
     if len(response) != 2:
         raise Exception('response has an unexpected number of fields')
@@ -94,29 +75,30 @@ def invoke(action, **params):
         raise Exception('response is missing required result field')
     if response['error'] is not None:
         raise Exception(response['error'])
-    return response['result']        
+    return response['result']
 
 
 def prepare_note(view, edit, nl, en, ru, tags, tr):
     return {
-            "deckName": "Dutch-Russian::Woorden::B1",
-            "modelName": "Dutch",
-            "fields": {
-                "Nederlands": nl,
-                "English": en,
-                "Russian": ru,
-                "Transcription": tr
-            },
-            "options": {
-                "allowDuplicate": False,
-                "duplicateScope": "deck",
-                "duplicateScopeOptions": {
-                    "deckName": "Dutch-Russian::Woorden::B1",
-                    "checkChildren": False
-                }
-            },
-            "tags": tags.split(' ')
-        }
+        "deckName": "Dutch-Russian::Woorden::B1",
+        "modelName": "Dutch",
+        "fields": {
+            "Nederlands": nl,
+            "English": en,
+            "Russian": ru,
+            "Transcription": tr
+        },
+        "options": {
+            "allowDuplicate": False,
+            "duplicateScope": "deck",
+            "duplicateScopeOptions": {
+                "deckName": "Dutch-Russian::Woorden::B1",
+                "checkChildren": False
+            }
+        },
+        "tags": tags.split(' ')
+    }
+
 
 class GetTranscriptionCommand(sublime_plugin.TextCommand):
 
@@ -154,8 +136,7 @@ class SendToAnkiCommand(sublime_plugin.TextCommand):
             # result = invoke('addNote', note=prepare_note(self.view, edit))
             # self.view.show_popup("<b>Result</b><br>" + str(result), sublime.HIDE_ON_MOUSE_MOVE_AWAY)
         except Exception as e:
-             print_in_panel(self.view, edit, "*** Error:" + str(e))
-
+            print_in_panel(self.view, edit, "*** Error:" + str(e))
 
 
 class ShowHelpCommand(sublime_plugin.TextCommand):
@@ -175,6 +156,5 @@ Fields order: <ul>
 <li>Russian
 </ul>
 """, 0, -1, 500, 300)
-
 
 # show_popup(content, <flags>, <location>, <max_width>, <max_height>, <on_navigate>, <on_hide>)
